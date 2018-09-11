ec31de723fa15bdf43f634bb4b0633a9dd23cefe
Igor Popov <Igor.Popov@transwareag.com>
2018-09-11 12:09:03 +0300

    Core

---
+++ Initterm.cpp
@@ -0,0 +1,21 @@
+#include "../InitTerm.h"
+#include "../core.h"
+#include "MemoryWindows.h"
+
+namespace Nirvana {
+namespace Core {
+
+void initialize_memory ()
+{
+	Windows::MemoryWindows::initialize ();
+	g_protection_domain_memory = Windows::MemoryWindows::_this ();
+}
+
+void terminate_memory ()
+{
+	g_protection_domain_memory = Memory::_nil ();
+	Windows::MemoryWindows::terminate ();
+}
+
+}
+}
