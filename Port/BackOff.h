f583b62a0e68903edc4f1019700236a6ee48fff0
Igor Popov <silver.popov@gmail.com>
2020-11-06 08:29:43 +0300

    BackOff improvement.

--- Port/BackOff.h
+++ Port/BackOff.h
@@ -1,8 +1,13 @@
 #ifndef NIRVANA_CORE_PORT_BACKOFF_H_
 #define NIRVANA_CORE_PORT_BACKOFF_H_
 
+#include <Nirvana/Nirvana.h>
+
 extern "C" __declspec (dllimport)
 void __stdcall Sleep (unsigned long dwMilliseconds);
+
+extern "C" __declspec (dllimport)
+int __stdcall SwitchToThread (void);
 
 namespace Nirvana {
 namespace Core {
@@ -11,9 +16,17 @@ namespace Port {
 class BackOff
 {
 protected:
-	static void sleep (unsigned hint)
+	/// A number of iterations when we should call yield ().
+	/// SwitchToThread () experiences the expensive cost of a context switch, which can be 10000+ cycles.
+	/// It also suffers the cost of ring 3 to ring 0 transitions, which can be 1000+ cycles.
+	static const UWord ITERATIONS_YIELD = 11000;
+
+	/// Maximal number of iterations.
+	static const UWord ITERATIONS_MAX = 1000000;
+
+	static void yield (UWord iterations)
 	{
-		::Sleep (hint ? 1 : 0);
+		::SwitchToThread ();
 	}
 };
 
