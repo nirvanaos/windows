915ba70c4b71103bed5e8506f694414f12089726
silver-popov <silver.popov@gmail.com>
2021-03-30 22:00:25 +0300

    Prepare for publish.

--- Source/SchedulerMaster.h
+++ Source/SchedulerMaster.h
@@ -1,7 +1,27 @@
-// Nirvana project
-// Windows implementation.
-// SchedulerMaster class. 
-
+/// \file
+/*
+* Nirvana Core. Windows port library.
+*
+* This is a part of the Nirvana project.
+*
+* Copyright (c) 2021 Igor Popov.
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+* Send comments and/or bug reports to:
+*  popov.nirvana@gmail.com
+*/
 #ifndef NIRVANA_CORE_WINDOWS_SCHEDULERMASTER_H_
 #define NIRVANA_CORE_WINDOWS_SCHEDULERMASTER_H_
 
@@ -58,6 +78,7 @@ private:
 	uintptr_t executor_;
 };
 
+/// SchedulerMaster class. 
 class SchedulerMaster :
 	public SchedulerBase <SchedulerMaster>,
 	public PostOffice <SchedulerMaster, sizeof (SchedulerMessage::Buffer), SCHEDULER_THREAD_PRIORITY>,
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Nirvana Port ibrary for the Windows host
+
+This is a part of the Nirvana project.
