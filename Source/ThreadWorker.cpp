bd9d021a68ee626a2753cb0e83738d97d88018b3
Igor Popov <popov.nirvana@gmail.com>
2021-05-05 17:44:42 +0300

    Rename Core_var to Core_ref

--- Source/ThreadWorker.cpp
+++ Source/ThreadWorker.cpp
@@ -45,7 +45,7 @@ unsigned long __stdcall ThreadWorker::thread_proc (ThreadWorker* _this)
 
 struct ThreadWorker::MainNeutralFiberParam
 {
-	Core_var <ExecDomain> main_domain;
+	Core_ref <ExecDomain> main_domain;
 };
 
 void CALLBACK ThreadWorker::main_neutral_fiber_proc (MainNeutralFiberParam* param)
