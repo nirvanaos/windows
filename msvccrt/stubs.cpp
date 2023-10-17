0465e857b5e89bc3eea7e9247e38b849aefb1e26
Igor Popov <popov.nirvana@gmail.com>
2023-10-17 09:55:52 +0300

    Extend msvccrt.lib

--- msvccrt/stubs.cpp
+++ msvccrt/stubs.cpp
@@ -1,3 +1,4 @@
+#include <corecrt_startup.h>
 #include <Windows.h>
 
 extern "C" int __cdecl _seh_filter_dll (
@@ -7,3 +8,9 @@ extern "C" int __cdecl _seh_filter_dll (
 {
   return EXCEPTION_CONTINUE_SEARCH;
 }
+
+extern "C" errno_t __cdecl _configure_narrow_argv (_crt_argv_mode const mode)
+{
+  return 0;
+}
+
